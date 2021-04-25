c0b3953bf28038f872bda0191278faa6eea2aaef
UiooAuto <zhangjin6793@hotmail.com>
2021-04-25 15:18:55 +0800

    Initial commit

---
+++ BackLightLine/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace BackLightLine
+{
+    class Program
+    {
+        static Work work1 = new Work();
+        static Work work2 = new Work();
+        static Work work3 = new Work();
+        bool startSan = true;
+
+        static void Main(string[] args)
+        {
+            //var plcIp1 = "192.168.0.3";
+            //var plcIp2 = "192.168.0.4";
+            //var plcIp3 = "192.168.0.5";
+            var plcIp1 = "127.0.0.1";
+            var plcIp2 = "127.0.0.1";
+            var plcIp3 = "127.0.0.1";
+            var plcPort = 12289;
+            var localIp = "127.0.0.1";
+            var localPort = 5024;
+
+            string cam1CmdAds = "D6030 01";
+            string cam1ResAds = "D6032 01";
+            string cam2CmdAds = "D8030 01";
+            string cam2ResAds = "D8032 01";
+            string cam3CmdAds = "D10030 01";
+            string cam3ResAds = "D10032 01";
+
+            Socket plc1Socket = InspectUtils.connectToTarget(plcIp1, plcPort);
+            Socket plc2Socket = InspectUtils.connectToTarget(plcIp2, plcPort);
+            Socket plc3Socket = InspectUtils.connectToTarget(plcIp3, plcPort);
+            Socket localSocket = InspectUtils.connectToTarget(localIp, localPort);
+
+            work1.plcSocket = plc1Socket;
+            work1.localSocket = localSocket;
+            work1.camCmdAds = cam1CmdAds;
+            work1.camResAds = cam1ResAds;
+            work1.san = true;
+
+            work2.plcSocket = plc2Socket;
+            work2.localSocket = localSocket;
+            work2.camCmdAds = cam2CmdAds;
+            work2.camResAds = cam2ResAds;
+            work2.san = true;
+
+            work3.plcSocket = plc3Socket;
+            work3.localSocket = localSocket;
+            work3.camCmdAds = cam3CmdAds;
+            work3.camResAds = cam3ResAds;
+            work3.san = true;
+
+            Thread thread1 = new Thread(new ThreadStart(work1.go));
+            Thread thread2 = new Thread(new ThreadStart(work2.go));
+            Thread thread3 = new Thread(new ThreadStart(work3.go));
+
+            thread1.Name = "cam1";
+            thread2.Name = "cam2";
+            thread3.Name = "cam3";
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+    }
+}
---
+++ BackLightLine/Work.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace BackLightLine
+{
+    class Work
+    {
+        public Socket plcSocket;
+        public Socket localSocket;
+        public string readCmd = "01WRD ";
+        public string writeCmd = "01WWR ";
+        public string camCmdAds;
+        public string camResAds;
+        public byte[] resBytes = new byte[1024];
+        public string result;
+        public bool san = true;
+
+        public void go()
+        {
+            Thread currentThread = Thread.CurrentThread;
+            lock (this)
+            {
+                while (san)
+                {
+                    result = "";
+                    var plcCmdStr = InspectUtils.receiveDataFromTarget(plcSocket,resBytes);
+                    var plcCmd = int.Parse(plcCmdStr);
+                    /*
+                     * 读取PLC命令
+                     */
+                    /*int plcCmd = getPlcCmd(plcSocket, camCmdAds);*/
+                    Console.WriteLine(plcCmd + "---" + currentThread.Name);
+                    if (plcCmd == 1)
+                    {
+                        result = readInspect(1);
+
+                    }
+                    else if (plcCmd == 2)
+                    {
+                        result = readInspect(2);
+                    }
+                    else if (plcCmd == 3)
+                    {
+                        result = readInspect(3);
+                    }
+                    if (result == "1")
+                    {
+                        setPlcCmd(plcSocket, camResAds, "0001");
+                    }
+                    else if (result == "2")
+                    {
+                        setPlcCmd(plcSocket, camResAds, "0002");
+                    }
+                    Thread.Sleep(200);
+                }
+            }
+
+        }
+
+        private string setPlcCmd(Socket socket, string plcAddress, string setResult)
+        {
+            string rtn = InspectUtils.sendCmdToTarget(socket, writeCmd + plcAddress + " " + setResult + "\r\n");
+            return rtn;
+        }
+
+        private int getPlcCmd(Socket socket, string plcAddress)
+        {
+            string cmd = InspectUtils.sendCmdToTarget(socket, readCmd + plcAddress + "\r\n");
+            Console.WriteLine(cmd);
+            if (cmd == "11OK0001")
+            {
+                InspectUtils.sendCmdToTarget(plcSocket, "ok");
+                if (plcAddress == "D6030 01")
+                {
+                    return 1;
+                }
+                else if (plcAddress == "D8030 01")
+                {
+                    return 2;
+                }
+                else if (plcAddress == "D10030 01")
+                {
+                    return 3;
+                }
+            }
+            return 0;
+        }
+
+        private string readInspect(int camId)
+        {
+            string str = camId.ToString() + '\r';
+            InspectUtils.sendCmdToTarget(localSocket, str);
+            var receiveDataFromTarget = InspectUtils.receiveDataFromTarget(localSocket, resBytes);
+            Console.WriteLine(receiveDataFromTarget+"---Inspect");
+            return receiveDataFromTarget;
+        }
+    }
+}
